48b91f51aacc3bdba45aad55cd934b837a917ff0
Henrique <henriqueheron1@gmail.com>
2025-12-19 19:40:19 -0300

    refactor(ls.c): make it work w/o args

--- tools/ls.c
+++ tools/ls.c
@@ -2,8 +2,14 @@
 #include <dirent.h>
 
 void ls(const char* dir_name) {
-    DIR* dir = opendir(dir_name);
     struct dirent* dir_entry;
+    DIR* dir;
+
+    if (dir_name == NULL) {
+        dir = opendir(".");
+    } else {
+        dir = opendir(dir_name);
+    }
 
     if (dir == NULL) {
         perror("Could not open dir");
